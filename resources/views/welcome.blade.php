88f822e2e26de515bbd55c801f7d25211b9c832f
blindmelon07 <bryandetera07@gmail.com>
2025-05-19 20:36:44 -0700

    added connection to chatify shit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -462,10 +462,12 @@
                                         </span>
                                     </div>
                                     <div class="space-y-3">
-                                        <button class="w-full px-4 py-2 text-sm text-white bg-indigo-600 rounded-lg hover:bg-indigo-700">
-                                            <i class="mr-2 fas fa-comments"></i>
-                                            Contact Seller
-                                        </button>
+                                      <button
+    class="w-full px-4 py-2 text-sm text-white bg-indigo-600 rounded-lg hover:bg-indigo-700"
+    @click="window.location.href = '/chatify/{{ $product->user_id }}'">
+    <i class="mr-2 fas fa-comments"></i>
+    Contact Seller
+</button>
                                         <button class="w-full px-4 py-2 text-sm text-indigo-600 border border-indigo-600 rounded-lg hover:bg-indigo-50">
                                             <i class="mr-2 fas fa-heart"></i>
                                             Save Item
